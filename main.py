8c087d68a3abfffc187e6ba7cec8060dd51255ad
macgroigar <stephenmacgregor4@gmail.com>
2022-08-17 20:11:12 +0100

    logic for tip calculator

--- main.py
+++ main.py
@@ -4,3 +4,13 @@
 
 # round the result to 2 decimal places
 
+print("Welcome to the tip calaulator!")
+bill = float(input("What was the total bill £"))
+tip = int(input("How much tip would you like to give? 10, 15, 20"))
+people = int(input("How many people split the bill"))
+tip_as_percent = tip / 100 
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+print(f"Each person should pay {final_amount} pounds")
